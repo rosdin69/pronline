12342210f0fdf2ee3886aebb3afec735552deddb
rosdin69 <122266543+rosdin69@users.noreply.github.com>
2025-03-02 13:40:07 +0100

    Update database_session.py

--- app/core/database_session.py
+++ app/core/database_session.py
@@ -5,7 +5,6 @@
 # for pool size configuration:
 # https://docs.sqlalchemy.org/en/20/core/pooling.html#sqlalchemy.pool.Pool
 
-
 from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
@@ -20,15 +19,18 @@ from app.core.config import get_settings
 def new_async_engine(uri: URL) -> AsyncEngine:
     return create_async_engine(
         uri,
-        pool_pre_ping=True,
-        pool_size=5,
-        max_overflow=10,
-        pool_timeout=30.0,
-        pool_recycle=600,
+        pool_pre_ping=True,  # Verifica que la conexión esté activa antes de usarla
+        pool_size=1,  # SQLite solo permite una conexión de escritura a la vez
+        max_overflow=0,  # Evita conexiones adicionales
+        pool_timeout=30.0,  # Tiempo de espera para obtener una conexión
+        pool_recycle=600,  # Recicla las conexiones después de 600 segundos
     )
 
 
+# Crea el motor asíncrono usando la URL de la base de datos desde la configuración
 _ASYNC_ENGINE = new_async_engine(get_settings().sqlalchemy_database_uri)
+
+# Crea una fábrica de sesiones asíncronas
 _ASYNC_SESSIONMAKER = async_sessionmaker(_ASYNC_ENGINE, expire_on_commit=False)
 
 
